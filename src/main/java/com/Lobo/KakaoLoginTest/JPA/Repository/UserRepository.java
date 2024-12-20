b413c12386f35a27037fcecb5528d63971a42d91
son0307 <son03071@naver.com>
2024-12-20 22:07:45 +0900

    최종 코드 업로드

--- src/main/java/com/Lobo/KakaoLoginTest/JPA/Repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.Lobo.KakaoLoginTest.JPA.Repository;
-
-import com.Lobo.KakaoLoginTest.JPA.Entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends JpaRepository<User, String> {
-}
